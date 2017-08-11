fffdb402fa090d1d1dfc8824452bdf6aba9881f6
Harshini01 <sri.kasinadhuni@gmail.com>
2017-08-11 23:58:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,21 +4,89 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles={
+    'mindspeaks' :{
+    title:'mindspeaks | Harshini',
+    heading :'My mind Speaks',
+    date:'August 11 2018',
+    content:` <p>
+                    To Infinity and beyond--Toy story
+                </p>
+                <p>
+                    To Infinity and beyond--Toy story
+                </p>
+                <p>
+                    To Infinity and beyond--Toy story
+                </p>`
+    
+},
+    'article-two':{title:'article-two | Harshini',
+    heading :'My Second article',
+    date:'August 13 2018',
+    content:` <p>
+                    hungry duck
+                </p>
+                <p>
+                    hangry
+                </p>
+                <p>
+                    really happy
+                </p>`},
+    'article-three':{
+        title:'article-three | Harshini',
+        heading :'My third article',
+        date:'August 16 2018',
+        content:` <p>
+                        globalisation
+                    </p>
+                    <p>
+                        sleep
+                    </p>
+                    <p>
+                        plumcake
+                    </p>`
+            
+    }
+};
+function createTemplate(data)
+{
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate=`<html>
+        <head>
+            <title>${title}</title>
+            <meta name ="viewport" conent="width-device-width , initial-sacle=1"/>
+             <link href="/ui/style.css" rel="stylesheet" />
+            <style>
+                
+            </style>
+        </head>
+        <body>
+            <div class = "container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>${date}</div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+    </html>`
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/mindspeaks',function(req,res)
+app.get('/:articleName',function(req,res)
 {
-     res.sendFile(path.join(__dirname, 'ui', 'mindspeaks.html'));
-    
-});
-app.get('/article-two',function(req,res)
-{ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res)
-{ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+     res.send(createTemplate(articles[articleName]));
     
 });
 app.get('/ui/style.css', function (req, res) {
