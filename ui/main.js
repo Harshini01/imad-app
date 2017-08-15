0625c0813b3412ad22f43786ad634c74b1e4557b
Harshini01 <sri.kasinadhuni@gmail.com>
2017-08-15 22:19:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//Change the text of main-text
 var element = document.getElementById('main-text');
 element.innerHTML = 'Harshini Loves herself';
+
+//Move the image of madi on click
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
