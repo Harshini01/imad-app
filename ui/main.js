29f55604a9131d668cd292a869daf33425bb28c0
Harshini01 <sri.kasinadhuni@gmail.com>
2017-08-16 01:38:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,22 +17,14 @@ img.onclick = function(){
 
 //Change texxt on Button click
 
-var button = document.getElementById('counter');
+var button = document.getElementById("counter");
+var counter = 0;
 button.onclick = function(){
-    var request = new XMLHttpRequest();
+   /* var request = new XMLHttpRequest();
     request.onReadyStateChange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('value');
-                span.innerHTML = counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET','http://srikasinadhuni.imad.hasura-app.io/counter',true);
-    request.send(null);
+        if(request.readyState === XMLHttpRequestDONE)
+    }*/
+    counter = counter+1;
+    var span = document.getElementById("value");
+    span.innerHTML = counter.toString();
 };
-   
