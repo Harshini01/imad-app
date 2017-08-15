6c8b0fe0ccd644dc63b2eaf872cea92ea308719d
Harshini01 <sri.kasinadhuni@gmail.com>
2017-08-16 01:32:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //Change the text of main-text
 var element = document.getElementById('main-text');
@@ -13,4 +13,27 @@ function moveright(){
 }
 img.onclick = function(){
   var interval = setInterval(moveright,50);
+};*/
+
+//Change texxt on Button click
+
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onReadyStateChange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById("value");
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET','http://srikasinadhuni.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
+   
